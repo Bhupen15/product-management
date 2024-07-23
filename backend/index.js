4de2dbc5f8270db9992ad6f44f343b06343c8833
Bhupendra Choudhary <80590016+Bhupen15@users.noreply.github.com>
2024-07-23 16:09:51 +0530

    third commit

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 });
 
 app.use('/api', productRoutes);
-
+//hello
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
